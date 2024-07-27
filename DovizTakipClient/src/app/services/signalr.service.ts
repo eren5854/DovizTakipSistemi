ec2fd812000e9c4af768c1735c7dbe3c58c89411
eren5854 <ihsanerendelibas@hotmail.com>
2024-07-27 06:45:28 +0300

    .

---
+++ DovizTakipClient/src/app/services/signalr.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import * as signalR from '@microsoft/signalr'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignalrService {
+
+  hub: signalR.HubConnection | undefined;
+  constructor() { }
+
+  startConnection(callBack: () => void) {
+    this.hub = new signalR.HubConnectionBuilder()
+      .withUrl("https://localhost:44392/sensor-hub")
+      .build();
+
+    this.hub
+      .start()
+      .then(() => {
+        console.log("Connection started");
+        callBack();
+      })
+      .catch((err: any) => console.log(err));
+  }
+}
